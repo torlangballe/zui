2f173934ac21c474d2187ea6dddc47e3c285becd
Tor <tor.langballe@bridgetech.tv>
2020-12-05 00:40:01 +0100

    removing github.com/kbinani/screenshot usage

--- Screen_windows.go
+++ Screen_windows.go
@@ -1,24 +1,20 @@
 package zui
 
-import (
-	"github.com/kbinani/screenshot"
-	"github.com/torlangballe/zutil/zgeo"
-	"github.com/torlangballe/zutil/zlog"
-)
+// "github.com/kbinani/screenshot"
 
 func ScreenGetAll() (screens []Screen) {
-	n := screenshot.NumActiveDisplays()
-	if n <= 0 {
-		zlog.Fatal(nil, "Active display not found")
-	}
-	for i := 0; i < n; i++ {
-		var s Screen
-		bounds := screenshot.GetDisplayBounds(i)
-		s.Rect = zgeo.RectFromGoRect(bounds)
-		s.IsMain = (i == 0)
-		s.Scale = 1
-		s.SoftScale = 1
-		screens = append(screens, s)
-	}
+	// n := screenshot.NumActiveDisplays()
+	// if n <= 0 {
+	// 	zlog.Fatal(nil, "Active display not found")
+	// }
+	// for i := 0; i < n; i++ {
+	// 	var s Screen
+	// 	bounds := screenshot.GetDisplayBounds(i)
+	// 	s.Rect = zgeo.RectFromGoRect(bounds)
+	// 	s.IsMain = (i == 0)
+	// 	s.Scale = 1
+	// 	s.SoftScale = 1
+	// 	screens = append(screens, s)
+	// }
 	return
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/disintegration/imaging v1.6.2
 	github.com/fogleman/gg v1.3.0
 	github.com/garyburd/redigo v1.6.2
-	github.com/kbinani/screenshot v0.0.0-20191211154542-3a185f1ce18f
 	github.com/lxn/walk v0.0.0-20201125094449-2a61ddb5a2b8
 	github.com/torlangballe/zutil v0.0.0-20201126235223-ae6e8fe16b25
 )
