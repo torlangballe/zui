b400acb2595bbb007875dcba951a446e01a08b07
Tor Langballe <tor.langballe@bridgetech.tv>
2023-05-25 13:37:59 +0200

    Fix call to zlog.Fatal, missing error

--- zmenu/MenuView_js.go
+++ zmenu/MenuView_js.go
@@ -161,7 +161,7 @@ func (v *MenuView) SetSelectedHandler(handler func()) {
 
 func (v *MenuView) SetFont(font *zgeo.Font) {
 	if font.Size != 14 {
-		zlog.Fatal("can't set menu view font size to anything except 14 in js")
+		zlog.Fatal(nil, "can't set menu view font size to anything except 14 in js")
 	}
 	f := *font
 	if zdevice.WasmBrowser() == "Safari" {
