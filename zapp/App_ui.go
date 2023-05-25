95c976659d475d694b0b53c9ad66ead2c0a7e719
Tor Langballe <tor.langballe@bridgetech.tv>
2023-05-25 16:27:10 +0200

    Move NewCurrentTimeLabel from zguiutil

--- zapp/App_ui.go
+++ zapp/App_ui.go
@@ -8,13 +8,20 @@ import (
 	"time"
 
 	"github.com/torlangballe/zui"
+	"github.com/torlangballe/zui/zcolor"
 	"github.com/torlangballe/zui/zdom"
+	"github.com/torlangballe/zui/zlabel"
 	"github.com/torlangballe/zui/zwidget"
+	"github.com/torlangballe/zui/zwindow"
 	"github.com/torlangballe/zutil/zbool"
+	"github.com/torlangballe/zutil/zgeo"
 	"github.com/torlangballe/zutil/zkeyvalue"
+	"github.com/torlangballe/zutil/zlocale"
 	"github.com/torlangballe/zutil/zlog"
 	"github.com/torlangballe/zutil/zrest"
-	zrpc "github.com/torlangballe/zutil/zrpc"
+	"github.com/torlangballe/zutil/zrpc"
+	"github.com/torlangballe/zutil/ztimer"
+
 	"github.com/torlangballe/zutil/ztime"
 )
 
@@ -90,4 +97,47 @@ func GetTimeInfoFromServer() error {
 	ServerTimeDifference = t.Sub(time.Now()) / 2
 	// zlog.Info("Got Time:", info.JSISOTimeString, time.Now(), "shift:", ServerTimeDifference, "offset:", ztime.ServerTimezoneOffsetSecs)
 	return nil
+}
+
+func NewCurrentTimeLabel() *zlabel.Label {
+	label := zlabel.New("")
+	label.SetObjectName("time")
+	label.SetFont(zgeo.FontDefault().NewWithSize(zgeo.FontDefaultSize - 2))
+	label.SetColor(zgeo.ColorNewGray(0.5, 1))
+	label.SetMinWidth(145)
+	label.SetTextAlignment(zgeo.Right)
+	label.SetPressedDownHandler(func() {
+		toggleTimeZoneMode(label)
+	})
+	updateCurrentTime(label)
+	ztimer.RepeatForever(1, func() {
+		updateCurrentTime(label)
+	})
+	return label
+}
+
+func toggleTimeZoneMode(label *zlabel.Label) {
+	d := !zlocale.DisplayServerTime.Get()
+	zlog.Info("toggleTimeZoneMode", d)
+	zlocale.DisplayServerTime.Set(d)
+	updateCurrentTime(label)
+	zwindow.GetMain().Reload()
+}
+
+func updateCurrentTime(label *zlabel.Label) {
+	t := time.Now()
+	t = t.Add(ServerTimeDifference)
+	if ServerTimezoneName != "" {
+		loc, _ := time.LoadLocation(ServerTimezoneName)
+		if loc != nil {
+			t = t.In(loc)
+		}
+	}
+	str := ztime.GetNice(time.Now(), true)
+	label.SetText(str)
+	col := zgeo.ColorBlack
+	if ServerTimeDifference > time.Second*4 {
+		col = zgeo.ColorRed
+	}
+	label.SetColor(col.WithOpacity(0.7))
 }
