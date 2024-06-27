8098ed7fbd93fd5c82e7c1da98fc648bb104731b
Tor Langballe <tor.langballe@bridgetech.tv>
2024-06-27 09:33:35 +0200

    SearchView: Add SetValueHandler() that calls underlying TextView's SetValueHandler

--- ztext/SearchField_ui.go
+++ ztext/SearchField_ui.go
@@ -39,6 +39,16 @@ func SearchFieldNew(style Style, chars int) *SearchField {
 	return s
 }
 
+func (v *SearchField) SetValueHandler(id string, handler func(edited bool)) {
+	v.TextView.SetValueHandler(id, handler)
+}
+
+// func (v *SearchField) CalculatedSize(total zgeo.Size) zgeo.Size {
+// 	s := v.TextView.CalculatedSize(total)
+// 	s.H = v.MinSize().H
+// 	return s
+// }
+
 func (v *SearchField) Text() string {
 	return v.TextView.Text()
 }
