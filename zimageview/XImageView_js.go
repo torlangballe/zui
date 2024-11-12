3fc47b95bb3be50e4ed6c2a7a3d2b67358575100
Tor Langballe <tor.langballe@bridgetech.tv>
2024-11-12 09:52:43 +0100

    Using new XImage, made it work

--- zimageview/XImageView_js.go
+++ zimageview/XImageView_js.go
@@ -11,13 +11,8 @@ import (
 
 type XImageView struct {
 	zview.NativeView
-	fitSize   zgeo.Size
+	minSize   zgeo.Size
 	alignment zgeo.Alignment
-	// imageCorner float64
-	// strokeWidth float64
-	// strokeColor zgeo.Color
-	// strokeInset bool
-	// EmptyColor  zgeo.Color
 }
 
 func XNew(name, imagePath string, fitSize zgeo.Size) *XImageView {
@@ -30,45 +25,14 @@ func (v *XImageView) Init(view zview.View, name string, imagePath string, fitSiz
 	v.MakeJSElement(view, "img")
 	v.SetCanTabFocus(false)
 	v.alignment = zgeo.Center | zgeo.Proportional
+	v.SetURL(imagePath)
 }
-
-// func (v *ImageView) SetPressToShowImage(on bool) {
-// 	if on {
-// 		v.SetPressedHandler(func() {
-// 			if v.image != nil {
-// 				path := v.image.Path
-// 				if !zhttp.StringStartsWithHTTPX(path) {
-// 					path = zstr.Concat("/", zrest.AppURLPrefix, path)
-// 				}
-// 				nv := New(v.image, v.image.Path, zgeo.SizeNull)
-// 				att := zpresent.AttributesNew()
-// 				att.Modal = true
-// 				att.ModalCloseOnOutsidePress = true
-// 				zpresent.PresentTitledView(nv, path, att, nil, nil)
-// 			}
-// 		})
-// 	} else {
-// 		v.SetPressedHandler(nil)
-// 	}
-// }
-
-// func (v *ImageView) SetStroke(width float64, c zgeo.Color, inset bool) {
-// 	v.strokeWidth = width
-// 	v.strokeColor = c
-// 	v.strokeInset = inset
-// 	v.Expose()
-// }
-
-// func (v *ImageView) SetImageCorner(radius float64) {
-// 	v.imageCorner = radius
-// 	v.Expose()
-// }
 
 func (v *XImageView) CalculatedSize(total zgeo.Size) (s, max zgeo.Size) {
 	// zlog.Info("IV CS", v.Hierarchy(), s, p, v.image != nil, zlog.GetCallingStackString())
 	// margSize := v.Margin().Size
-	if !v.fitSize.IsNull() {
-		s = v.fitSize
+	if !v.minSize.IsNull() {
+		s = v.minSize
 		// s = s.ShrunkInto(v.fitSize.Plus(margSize))
 	}
 	// if !v.minSize.IsNull() {
@@ -84,12 +48,12 @@ func (v *XImageView) CalculatedSize(total zgeo.Size) (s, max zgeo.Size) {
 	return s, s
 }
 
-func (v *XImageView) FitSize() zgeo.Size {
-	return v.fitSize
+func (v *XImageView) MinSize() zgeo.Size {
+	return v.minSize
 }
 
-func (v *XImageView) SetFitSize(s zgeo.Size) {
-	v.fitSize = s
+func (v *XImageView) SetMinSize(s zgeo.Size) {
+	v.minSize = s
 }
 
 // func (v *XImageView) Alignment() zgeo.Alignment {
