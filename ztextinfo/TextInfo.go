4631374f32e80cd5b277b2c44198ab2c3249b84c
Tor Langballe <tor.langballe@bridgetech.tv>
2025-01-06 09:19:15 +0100

    ztextinfo: Adjust multi-line height calculation

--- ztextinfo/TextInfo.go
+++ ztextinfo/TextInfo.go
@@ -218,7 +218,7 @@ func (ti *Info) GetBounds() (size zgeo.Size, allLines []string, widths []float64
 		}
 		//	count = maxLines
 		if count > 1 || ti.IsMinimumOneLineHight {
-			size.H = float64(ti.Font.LineHeight()*1.2) * float64(zint.Max(count, 1))
+			size.H = float64(ti.Font.LineHeight()*1.3) * float64(zint.Max(count, 1))
 		}
 		// zlog.Info("BOUNDS:", size, count, ti.Text)
 	}
